1c515ac831d13f2e17fd8fb95a74512166e84587
Nic <tianfang07@outlook.com>
2023-07-26 12:26:19 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <div class="app">Hi</div>
+  <div class="app">
+    <header>
+      <div class="title">
+          <img src="./assets/heart.svg" alt="site logo "/>
+          <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleSort('title')">按标题排列</button>
+        <button @click="handleSort('location')">按地点排列</button>
+        <button @click="handleSort('salary')">按薪资排列</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order"/>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import JobList from './components/JobList.vue'
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
+
 
 export default defineComponent({
   name: 'App',
   components: {
-  }
+    JobList
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "Bring Peace to Hyrule Field!", location: 'Hyrule Field, Central Hyrule', salary: 100, id: '1' },
+      { title:"Mattison's Independence", location:'Tarrey Town, Akkala', salary: 200, id:'2'},
+      { title:"The Horse Guard's Request", location:'Outskirt Stable, Central Hyrule', salary: 100, id:'3'},
+      
+    ])
+
+    const order = ref<OrderTerm>('title');
+
+    const handleSort = (term: OrderTerm) => {
+      console.log(term);
+      order.value = term;
+    }
+
+    return {jobs, handleSort, order}
+  }, 
 });
 </script>
 
 <style>
-
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
 </style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="job-list">
+    <p>Order by {{ order }}</p>
+    <ul >
+      <transition-group name="list" tag="ul">
+        <li v-for="job in filteredList" :key="job.id">
+          <h3>{{ job.title }} @{{ job.location }}</h3>
+          <div class="salary">
+            <img src="@/assets/rupee.svg" />
+
+            <p>{{ job.salary }} 卢比</p>
+          </div>
+          <div class="description">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, dignissimos? Magnam dolorum culpa earum quam, facere possimus ad, veritatis maxime repellendus deleniti minima itaque, placeat unde sequi corporis! Quos, aspernatur?
+            </p>
+          </div>
+        </li>
+      </transition-group>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue';
+import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm';
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
+    }
+  },
+  setup(props) {
+    
+    const filteredList = computed(() => {
+      // 扩展运算符 ... 对 props.jobs 数组进行浅拷贝
+      return [...props.jobs].sort((a: Job, b: Job) => {
+          if (a[props.order] < b[props.order]) {
+            return -1;
+          } else if (a[props.order] > b[props.order]) {
+            return 1;
+          } else {
+            return 0;
+          }
+      })
+    });
+    return {filteredList}
+  }
+})
+
+</script>
+
+<style scoped>
+  /* by Shuan  */
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0
+  }
+ .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+  .list-move {
+    transition: all 1s;
+  }
+
+/* by chatGPT  */
+
+.job-list li {
+  transition: transform 0.3s ease-in-out;
+}
+
+.job-list li:hover {
+  transform: translate(5px, -5px);
+}
+
+
+</style>
